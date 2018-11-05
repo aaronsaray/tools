ca0d93c66cee5756ee89d72d138daa0434753304
Aaron Saray <me@aaronsaray.com>
2018-11-05 17:44:30 -0600

    finished meme generator

--- src/Components/Meme/Meme.js
+++ src/Components/Meme/Meme.js
@@ -3,18 +3,21 @@ import {
   Container,
   Grid,
   Header,
-  Image,
   Input,
   Divider,
   Button,
   Segment,
-  Icon
+  Icon,
+  Message
 } from "semantic-ui-react";
 import PlaceholderImage from "./placeholder.png";
+import "./Meme.css";
 
 export default class Meme extends Component {
   state = {
-    image: PlaceholderImage
+    image: PlaceholderImage,
+    topText: "",
+    bottomText: ""
   };
 
   componentDidMount = () => {
@@ -57,7 +60,31 @@ export default class Meme extends Component {
     }
   };
 
-  // put a section that says this doens't handle errors
+  handleTopText = e => {
+    this.setState({
+      topText: e.target.value
+    });
+  };
+
+  handleBottomText = e => {
+    this.setState({
+      bottomText: e.target.value
+    });
+  };
+
+  handleResetText = () => {
+    this.setState({
+      topText: "",
+      bottomText: ""
+    });
+  };
+
+  handleResetAll = () => {
+    this.setState({
+      image: PlaceholderImage
+    });
+    this.handleResetText();
+  };
 
   render() {
     return (
@@ -67,10 +94,22 @@ export default class Meme extends Component {
             <Grid.Column>
               <Header as="h1">Meme Generator</Header>
             </Grid.Column>
+            <Grid.Column>
+              <Message negative>
+                <p>
+                  This has only been tested in <Icon name="chrome" /> - and only
+                  screenshots, not canvas/download
+                </p>
+              </Message>
+            </Grid.Column>
           </Grid.Row>
           <Grid.Row>
             <Grid.Column>
-              <Image fluid src={this.state.image} bordered />
+              <div className="ui bordered fluid image">
+                <div className="meme-text top">{this.state.topText}</div>
+                <div className="meme-text bottom">{this.state.bottomText}</div>
+                <img src={this.state.image} alt="meme" />
+              </div>
             </Grid.Column>
             <Grid.Column>
               <div className="ui input fluid">
@@ -87,9 +126,30 @@ export default class Meme extends Component {
                 Or <Icon name="paste" /> image
               </Segment>
               <Divider />
-              <Button basic fluid color="grey">
-                Reset
-              </Button>
+              <Input
+                label="Top"
+                fluid
+                placeholder="Headline text here"
+                onChange={this.handleTopText}
+                value={this.state.topText}
+              />
+              <Divider hidden />
+              <Input
+                label="Bottom"
+                fluid
+                placeholder="Optional footer text here"
+                onChange={this.handleBottomText}
+                value={this.state.bottomText}
+              />
+              <Divider />
+              <Button.Group fluid>
+                <Button basic color="grey" onClick={this.handleResetText}>
+                  Reset Text
+                </Button>
+                <Button basic color="red" onClick={this.handleResetAll}>
+                  Reset All
+                </Button>
+              </Button.Group>
             </Grid.Column>
           </Grid.Row>
         </Grid>
